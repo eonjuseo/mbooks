1ea86a08ac673cc8f60b4f2acf7c044cfe11d4d5
eonjuseo <ejooss@chungbuk.ac.kr>
2023-12-21 10:34:20 +0900

    Settings

---
+++ src/test/java/com/ll/mbooks/MbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.mbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
